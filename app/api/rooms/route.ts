6697bad8c9010b90d8618556732e75bf4219d3e1
chris <chrisperalta1998@gmail.com>
2024-03-15 21:54:05 -0700

    setup getallrooms post newroom

--- app/api/rooms/route.ts
+++ app/api/rooms/route.ts
@@ -1,6 +1,6 @@
 import { createEdgeRouter } from "next-connect";
 import { NextRequest } from "next/server";
-import { allRooms } from "@/backend/controllers/roomControllers";
+import { allRooms, newRoom } from "@/backend/controllers/roomControllers";
 import dbConnect from "@/backend/config/dbConnect";
 
 interface RequestContext {
@@ -14,7 +14,12 @@ const router = createEdgeRouter<NextRequest, RequestContext>();
 dbConnect();
 
 router.get(allRooms);
+router.post(newRoom);
 
 export async function GET(request: NextRequest, ctx: RequestContext) {
+  return router.run(request, ctx);
+}
+
+export async function POST(request: NextRequest, ctx: RequestContext) {
   return router.run(request, ctx);
 }
