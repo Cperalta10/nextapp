8a06bb85e5177530589870d7cf961ffe57174870
chris <chrisperalta1998@gmail.com>
2024-03-16 12:01:55 -0700

    added getroomdetails

--- backend/controllers/roomControllers.ts
+++ backend/controllers/roomControllers.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import Room from "../models/room";
 
+// Get all rooms => /api/rooms
 export const allRooms = async (req: NextRequest) => {
   const resPerPage: number = 8;
 
@@ -13,6 +14,7 @@ export const allRooms = async (req: NextRequest) => {
   });
 };
 
+// Creat new room => /api/rooms
 export const newRoom = async (req: NextRequest) => {
   const body = await req.json();
 
@@ -20,6 +22,28 @@ export const newRoom = async (req: NextRequest) => {
 
   return NextResponse.json({
     succes: true,
+    room,
+  });
+};
+
+// Get room details => /api/rooms/:id
+export const getRoomDetails = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const room = await Room.findById(params.id);
+
+  if (!room) {
+    return NextResponse.json(
+      {
+        message: "Room not found",
+      },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json({
+    success: true,
     room,
   });
 };
